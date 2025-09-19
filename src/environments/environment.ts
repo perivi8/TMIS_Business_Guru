11bacfd8cb2915ce29846c4b7aef6e0a37bde80c
perivi8 <perivihari8@gmail.com>
2025-09-19 17:52:49 +0530

    Describe your new changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api'  // Local development server
-};
+  apiUrl: 'https://tmis-business-guru-backend.onrender.com/api'
+};  // Local development server
